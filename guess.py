6ac026df764558aba9fa25206fc87e8f13d0e59c
zainamjad165 <98817942+zainamjad165@users.noreply.github.com>
2022-02-21 10:32:42 +0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -2,14 +2,13 @@ import random
 
 def guess(x):
     random_number=random.randint(1,x)
-    print(random_number)
     guess=0
     while guess != random_number:
-        guess=int(input(f"guess the number between 1 to {x}:  "))
+        guess=int(input(f"Guess a number between 1 to {x}:  "))
         if guess > random_number:
-            print ("the guessed number is too high")
+            print ("This number is too high")
         elif guess < random_number:
-            print ("the guessed number is too low")
-    print ("congrats you have guessed the number corect!")
+            print ("This number is too low")
+    print ("Congrats! Your guess is correct.")
 
 guess(10)                
